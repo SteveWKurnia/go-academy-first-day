8c2cd84e235de5cfa9bde181ba183bc43824c760
SteveWKurnia <45429692+SteveWKurnia@users.noreply.github.com>
2020-09-05 14:49:49 +0700

    Add state and event handling for follow button

--- src/App.js
+++ src/App.js
@@ -1,21 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import "./styles.css";
 
-export default function App() {
-  const username = "instagram";
+const Profile = (props) => {
+  //State and event handling
+  const [isFollowed, setIsFollowed] = useState(false);
 
   return (
-    <>
+    <React.Fragment>
       <div className="App">
-        <img src="https://via.placeholder.com/300" alt="profile picture" />
-        <div>@{username}</div>
-        <button>Follow</button>
+        <img src="https://via.placeholder.com/150" alt="profile picture" />
+        <div>@{props.username}</div>
+        <button onClick={() => setIsFollowed(!isFollowed)}>
+          {isFollowed ? "Unfollow" : "Follow"}
+        </button>
         <div>Posts</div>
         <div>Follower</div>
         <div>Following</div>
         <div>Bio</div>
-        <div id="root"></div>
+        <Post src="https://via.placeholder.com/300" />
       </div>
-    </>
+    </React.Fragment>
   );
+};
+
+const Post = (props) => (
+  <React.Fragment>
+    <img src={props.src} alt="posts" />
+  </React.Fragment>
+);
+
+export default function App() {
+  const username = "instagram";
+  return <Profile username={username} />;
 }
