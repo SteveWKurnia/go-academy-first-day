55d082efe31a390fc1fdb4750d37d94d4627e3ec
SteveWKurnia <45429692+SteveWKurnia@users.noreply.github.com>
2020-09-05 20:05:43 +0700

    End of first day

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
 import React, { useState } from "react";
 import "./styles.css";
 
+//Components
 const Profile = (props) => {
+  const { username } = props;
+  const postCount = 10;
+  const posts = [...Array(postCount)];
+
   //State and event handling
   const [isFollowed, setIsFollowed] = useState(false);
+  const [inputAccount, setInputAccount] = useState("");
 
   return (
     <React.Fragment>
       <div className="App">
-        <img src="https://via.placeholder.com/150" alt="profile picture" />
-        <div>@{props.username}</div>
+        <input
+          onChange={({ target: { value } }) => {
+            setInputAccount(value);
+          }}
+          placeholder="search profile"
+        />
+        <div>Search result for account: {inputAccount}</div>
+
+        <img src="https://via.placeholder.com/150" alt="profile" />
+        <div>@{username}</div>
         <button onClick={() => setIsFollowed(!isFollowed)}>
           {isFollowed ? "Unfollow" : "Follow"}
         </button>
-        <div>Posts</div>
+        <div>{postCount} Posts</div>
         <div>Follower</div>
         <div>Following</div>
         <div>Bio</div>
-        <Post src="https://via.placeholder.com/300" />
+        {postCount === 0 ? (
+          <div>No Posts</div>
+        ) : (
+          <div className="posts">
+            {posts.map((_, idx) => (
+              <Post
+                src="https://via.placeholder.com/300"
+                key={idx}
+                className="post"
+              />
+            ))}
+          </div>
+        )}
       </div>
     </React.Fragment>
   );
